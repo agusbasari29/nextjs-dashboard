672140aa1bcefec31721307f7162b2b2b07d0fd7
Agus Basari <agusbasari29@gmail.com>
2025-10-02 16:37:36 +0000

    add dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | XCode - Next.js Dashboard',
+    default: 'XCode - Next.js Dashboard',
+  },
+  description: 'A simple dashboard built with Next.js, TypeScript, and Tailwind CSS.',
+  metadataBase: new URL('https://xcode-dashboard.vercel.app'),
+  icons: {
+    icon: '/apple-icon.jpg',
+  },
+}
 
 export default function RootLayout({
   children,
